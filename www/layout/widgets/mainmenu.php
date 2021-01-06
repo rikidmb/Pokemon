c8c4670d68ab08360961e03a3e1356862bdbeedf
languages-vip <70502573+languages-vip@users.noreply.github.com>
2021-01-05 17:26:37 -0800

    Website
    Website original files

---
+++ www/layout/widgets/mainmenu.php
@@ -0,0 +1,28 @@
+<div class="mod-tab mod-menu">
+	<div class="module">
+		<div class="module-2">
+			<div class="module-3">
+				<div class="module-4">
+					<div class="module-5 deepest">
+						<div class="header-container-1">
+							<div class="header-container-2">
+								<div class="header-l"></div>
+								<div class="header-r"></div>
+								<h3 class="module"><span class="color">Main</span> Menu</h3>
+							</div>
+						</div>
+						<div style="overflow: hidden;">
+							<ul class="menu">
+								<li class="level1 item3 first"><a href="index.php" class="level1 item3 first"><span>Ultimas Noticias</span></a></li>
+								<li class="level1 item3 first"><a href="https://www.mediafire.com/" target ="_blank" class="level1 item3 first"><span>Download Cliente </span></a></li>
+								<li class="level1 item3 first"><a href="https://i.postimg.cc/zvwQvN0w/gHPA0.png" target ="_blank" class="level1 item3 first"><span>Mapa do Servidor</span></a></li>
+								<!-- <li class="level1 item3 first"><a href="?subtopic=archive" class="level1 item3 first"><span>News archive</span></a></li> 
+								<li class="level1 item7"><a href="?subtopic=lottery" class="level1 item7"><span>Lottery</span></a></li>-->
+							</ul>				
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
